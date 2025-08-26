5589464318d9f13276fb2b9682493344f44acfe7
Dev-Huthaifah <123631430+Dev-Huthaifah@users.noreply.github.com>
2025-08-26 18:58:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
     var typed = new Typed('#element', {
-    strings: ['رحلتك تبدأ من هنا...', 'احجز رحلتك الآن...'],
+    strings: ['رحلتك تبدأ من هنا...', 'احجز رحلتك الآن'],
     typeSpeed: 50,
     loop: true
+
     });
